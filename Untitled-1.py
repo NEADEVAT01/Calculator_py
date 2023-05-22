8b009c51580502ffa7bcaa97b73a99f5ddd7b8d0
NEADEVAT01 <pavel.melnik.2001@hotmail.com>
2023-05-22 15:40:55 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,9 +2,7 @@ while True:
     arg = ''
     arg=input("Enter Math Function: ")
     if (arg == 'q'): break
-    try:
-        print(arg,'=',eval(arg.replace('^','**'))) 
-    except NameError:
-        print('Invalid function, try again ಠ_ಠ')
+    if (arg.isdigit() == False): print('Invalid function, try again ಠ_ಠ')
+    else: print(arg,'=',eval(arg.replace('^','**')))   
     print('To exit type "q"')
 exit() 
